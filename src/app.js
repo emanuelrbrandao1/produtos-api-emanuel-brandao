c18c457bb2fdb2f9781d1755ccbb0aafc39b7b13
Emanuel Brandão <emanuelrbrandao@gmail.com>
2026-03-13 18:50:58 -0300

    feat(produtos): cria router e registra rotas no app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
 //Importa o express
 const express = require('express');
+const produtosRouter = require('./routes/produtos');
+
 //Declara o uso do express na variável app
 const app = express();
+
 //Permite que as requisições sejam interpretadas por JSON
 app.use(express.json());
+
 //Mostra a requisição e o caminho sendo utilizados
 app.use((req, res, next) => {  
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}`);  
     next();
 });
+
+//Conecta o Router ao servidor dizendo: "tudo que chegar com URL começando em /api/v1/produtos, encaminha pra esse Router resolver".
+app.use('/api/v1/produtos', produtosRouter);
+
 //Se algo der errado retorna 500 e uma mensagem de erro
 app.use((err, req, res, next) => {  
     res.status(500).json({ erro: err.message });
 });
+
 //Liga o servidor na porta 3000 e exibe confirmação no console
 app.listen(3000, () => console.log('API rodando na porta 3000'));
 
